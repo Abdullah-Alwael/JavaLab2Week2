25c4b22a6356a97af059b9866f21d9a6c108f30d
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-01 10:24:32 +0300

    Q1,2

---
+++ src/Lab2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Lab2 {
+    public static void main(String[] args) {
+//        1.Write a program to find all the longest word in a given dictionary.
+        System.out.println("Checking longest word in the array");
+
+        String[] longestWords = { "cat", "dog", "red", "is", "am" };
+        int longest = 0;
+
+        System.out.println("The array:");
+        for (String word:longestWords){
+            System.out.print("\""+word+"\", ");
+            if (word.length() > longest){
+                longest = word.length();
+            }
+        }
+        System.out.println();
+        System.out.println("Longest words are: ");
+        for (String word:longestWords){
+            if (word.length() == longest){
+                System.out.print("\""+word+"\", ");
+            }
+        }
+
+        //2. Write a program that displays the number of occurrences of an element in the array.
+
+        System.out.println("Checking the occurrences of an element in an array");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many elements in the array?");
+        int[] occurrences = new int[input.nextInt()];
+
+        for (int i = 0; i <= occurrences.length-1; i++) {
+            System.out.println("Please enter the "+(i+1)+" number:");
+            occurrences[i]= input.nextInt();
+        }
+
+        System.out.println("enter a number to check how many times it occurs:");
+        int occurringNumber = input.nextInt();
+        int occurs = 0;
+
+        for (int n:occurrences){
+            if (n == occurringNumber){
+                occurs++;
+            }
+        }
+
+        System.out.println(occurringNumber + " occurs "+occurs+" times");
+
+    }
+}
